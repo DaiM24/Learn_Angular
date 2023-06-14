b258a198bde5bd31ffd31c0f1c2a44e95f55c939
Daina_Mercado <Daiana_Mercado@itrsa.com.ar>
2023-06-13 23:11:23 -0300

    :constuction: Add 04-homework

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,8 +1,8 @@
 import './style.css';
 //import './topics/01-basic-types';
 //import './topics/02-object-interface';
-import './topics/03-funcions'
-
+//import './topics/03-funcions'
+import './topics/04-homework'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
  Hello Dai 😎
 `;
